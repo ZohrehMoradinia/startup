96f16cc917c0e56f0cf1d83fb1978c2bfb8ea9c5
ZohrehMoradinia <86517397+ZohrehMoradinia@users.noreply.github.com>
2025-05-30 20:23:02 +0100

    Update untitled33.py

--- untitled33.py
+++ untitled33.py
@@ -1,14 +1,14 @@
 
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 import numpy as np
-import pickle
+
 import pymoo
 from pymoo.core.problem import Problem
 from pymoo.algorithms.moo.nsga2 import NSGA2
